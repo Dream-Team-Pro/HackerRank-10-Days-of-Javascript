4f238e2d6416d49312a25c85cf3c73c6a5ee56b6
Dream-Team-Pro <mz1320021@yahoo.com>
2021-01-14 13:05:31 +0300

    resolved

--- Day 1 Arithmetic Operators.js
+++ Day 1 Arithmetic Operators.js
@@ -1,35 +1,3 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/**
-*   Calculate the area of a rectangle.
-*
-*   length: The length of the rectangle.
-*   width: The width of the rectangle.
-*   
-*	Return a number denoting the rectangle's area.
-**/
 function getArea(length, width) {
     let area;
     // Write your code here
@@ -37,14 +5,6 @@ function getArea(length, width) {
     return area;
 }
 
-/**
-*   Calculate the perimeter of a rectangle.
-*	
-*	length: The length of the rectangle.
-*   width: The width of the rectangle.
-*   
-*	Return a number denoting the perimeter of a rectangle.
-**/
 function getPerimeter(length, width) {
     let perimeter;
     // Write your code here
